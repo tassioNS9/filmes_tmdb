bbfc8a4ec53a793fe4da011d65bd23a7d32fe853
Tassio Neves <tassions@hotmail.com>
2026-05-13 19:53:13 -0300

    refactor: remove unused hooks and effects from App component

--- scr/App.js
+++ scr/App.js
@@ -1,14 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navigation from './navigation';
-import useTmdb from './hooks/useTmbd';
 
 function App() {
-  const { getNowPlaying } = useTmdb();
-
-  useEffect(() => {
-    getNowPlaying();
-  }, []);
-
   return <Navigation />;
 }
 
